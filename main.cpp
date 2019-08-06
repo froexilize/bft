82c245a67a921f17bac914a4f2dd5dc60a868cc8
Boris.Dergachov <admin@froexilize.com>
2019-08-06 17:10:16 +0300

    Refactor

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@ class General {
 					else {
 						v->second = ++v->second;
 						if(v->second >= q) {
-							std::cout << "Accept value " << v->first << " because the quorum limit (" << q << ")." << std::endl;
+							//std::cout << "Accept value " << v->first << " because the quorum limit (" << q << ")." << std::endl;
 							real[i] = v->first;
 							checked = true;
 							break;
@@ -63,7 +63,7 @@ class General {
 				}
 				if(!checked) {
 					real[i] = -1;
-					std::cout << "Failed to pass quorum limit (" << q << ")." << std::endl;
+					//std::cout << "Failed to pass quorum limit (" << q << ")." << std::endl;
 				}
 			}
 		}
@@ -104,36 +104,69 @@ class General {
 		}
 };
 
+void get_sample(const size_t r, const size_t size, std::vector<int> &out, size_t max_range = 0) {
+	if(max_range == 0) max_range = r;
+	size_t range_from = 0;
+	for(size_t remained = size; remained > 0; --remained) {
+		size_t range_to = r - remained + 1;
+		size_t range = range_to - range_from;
+		size_t next_traitor = range_from + gen() % (range > max_range ? max_range : range);
+		out.push_back(next_traitor);
+		range_from = next_traitor + 1;
+	}
+}
+
 int main() {
 	gen.seed(time(nullptr));
-	const int n = 4;
-	const int m = 1;
-	std::vector<General> generals;
-	generals.resize(n);
-	std::cout << "Random army size:" << std::endl;
+
+	// Init participants
+	const size_t num_participants = 1000;
+	const size_t num_traitors = num_participants / 4;
+	std::vector<General> participants;
+	participants.resize(num_participants);
+
+	std::vector<int> sample;
+	get_sample(num_participants, num_traitors, sample, 2 * num_participants / num_traitors);
+
+	for(auto s:sample) {
+		participants[s].betrayer = true;
+	}
+	std::cout << std::endl;
+
+	const size_t min_participants = 4;
+	// Form round
+	// Check round
+	// Commit round
+
+	sample.clear();
+	get_sample(participants.size(), 6, sample);
+	std::vector<General*> generals;
+	for(auto s:sample) {
+		generals.push_back(&participants[s]);
+	}
+
 	std::cout << "( ";
 	for(auto g:generals) {
-		std::cout << g.army_size << " ";
+		std::cout << g->army_size << (g->betrayer ? "!" : " ") << " ";
 	}
 	std::cout << ")" << std::endl;
-	generals[2].betrayer = true;
+	generals[2]->betrayer = true;
 // Step 1: ask army sizes
 // Step 2: form vector
 	std::cout << "Ask army size. Generals have knowledge:" << std::endl;
-	std::cout << "Note: third general is betrayer" << std::endl;
 	for (size_t i = 0; i < generals.size(); ++i) {
 		for(size_t j = 0; j < generals.size(); ++j) {
-			generals[i].other.push_back( (i == j) ? generals[j].army_size : generals[j].ask_army() );
+			generals[i]->other.push_back( (i == j) ? generals[j]->army_size : generals[j]->ask_army() );
 		}
 		std::cout << i + 1 << ": ";
-		generals[i].print_other();
+		generals[i]->print_other();
 		std::cout << std::endl;
 	}
 // Step 3: ask vectors
 	std::cout << "Ask army vectors." << std::endl;
 	for(size_t i = 0; i < generals.size(); ++i) { // each general ...
 		for(size_t j = 0; j < generals.size(); ++j) { // ...ask another
-			generals[i].full.push_back( (i == j) ? generals[j].other : generals[j].ask_vector() );
+			generals[i]->full.push_back( (i == j) ? generals[j]->other : generals[j]->ask_vector() );
 		}
 	}
 
@@ -141,18 +174,17 @@ int main() {
 	std::cout << "  g1,         g2,         g3,         g4" << std::endl;
 	for(size_t i = 0; i < generals.size(); ++i) {
 		for(size_t j = 0; j < generals.size(); ++j) {
-			generals[j].print_full(i);
+			generals[j]->print_full(i);
 		}
 		std::cout << std::endl;
 	}
 // Step 4: Determine real values
 	for(size_t i = 0; i < generals.size(); ++ i) {
 		std::cout << "General " << i + 1 <<  " =======================" << std::endl;
-		generals[i].calc();
-		generals[i].print_calc();
+		generals[i]->calc();
+		generals[i]->print_calc();
 		std::cout << std::endl;
 	}
-
 	return 0;
 }
 
